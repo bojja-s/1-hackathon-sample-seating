794e966307f7ae32f2ba1f5a8409a80fe3bae919
bojja-s <bojja.s@hcl.com>
2018-06-14 21:07:09 +0530

    inital commit

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/WebApplication.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/WebApplication.java
@@ -2,24 +2,21 @@ package com.hcl.bootcamp.fs.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
-/**
- * Spring Boot Application launcher
- * 
- * @author Bojja
- *
- */
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebApplication.class);
+public class WebApplication {
+	// HACK: password= 18a2d64c-60e5-4561-9aa9-50873dd05220
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
 	}
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(WebApplication.class, args);
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("validation");
+		return source;
 	}
 }
