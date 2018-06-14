c59160c7ae9f3b0a8ad5333cfd16cbb5bc712e2d
bojja-s <bojja.s@hcl.com>
2018-06-15 00:43:24 +0530

    inital commit

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Seat.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Seat.java
@@ -7,59 +7,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-//import lombok.Data;
+import lombok.Data;
 
 @Entity
-//@Data
-@Table(name = "seat")
+@Data
 public class Seat {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
+	private Boolean available;
+	private String userName;
 
 	@JsonBackReference
-	@ManyToOne(fetch= FetchType.LAZY)
-	@JoinColumn(name="section_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "section_id")
 	private Section section;
-	
-	private Boolean available;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Section getSection() {
-		return section;
-	}
-
-	public void setSection(Section section) {
-		this.section = section;
-	}
-
-	public Boolean getAvailable() {
-		return available;
-	}
-
-	public void setAvailable(Boolean available) {
-		this.available = available;
-	}
-	
 }
--- src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Section.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Section.java
@@ -8,13 +8,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import lombok.Data;
+
 @Entity
-//@Data
-@Table(name = "section")
+@Data
 public class Section {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,37 +26,4 @@ public class Section {
 	@JsonManagedReference
 	@OneToMany(mappedBy = "section", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Seat> seats;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLayoutInfo() {
-		return layoutInfo;
-	}
-
-	public void setLayoutInfo(String layoutInfo) {
-		this.layoutInfo = layoutInfo;
-	}
-
-	public List<Seat> getSeats() {
-		return seats;
-	}
-
-	public void setSeats(List<Seat> seats) {
-		this.seats = seats;
-	}
-	
 }
