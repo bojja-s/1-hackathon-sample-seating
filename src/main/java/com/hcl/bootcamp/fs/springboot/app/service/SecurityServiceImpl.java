6e113bd5e8bab7970f296dd95138d276fa4179c0
bojja-s <bojja.s@hcl.com>
2018-06-14 22:03:51 +0530

    inital commit

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/service/SecurityServiceImpl.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/service/SecurityServiceImpl.java
@@ -3,8 +3,6 @@ package com.hcl.bootcamp.fs.springboot.app.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,9 +12,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityServiceImpl implements SecurityService{
 	
-	@Qualifier("authenticationManagerBean")
-    @Autowired
-    private AuthenticationManager authenticationManager;
+//	@Qualifier("authenticationManagerBean")
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -38,7 +36,7 @@ public class SecurityServiceImpl implements SecurityService{
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
 
-        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+       // authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
         if (usernamePasswordAuthenticationToken.isAuthenticated()) {
             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
