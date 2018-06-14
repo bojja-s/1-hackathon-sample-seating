5f3acbd5e1d6f91094b3bb4440159488bf5bbb8a
bojja-s <bojja.s@hcl.com>
2018-06-14 19:39:01 +0530

    inital commit

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/WebApplication.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/WebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class WebApplication {
 	//HACK: password= 18a2d64c-60e5-4561-9aa9-50873dd05220
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
