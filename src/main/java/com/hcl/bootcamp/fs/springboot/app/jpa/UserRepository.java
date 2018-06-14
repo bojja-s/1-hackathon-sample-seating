2e9c51fef777439a448cfaba98299cb4a415a1c2
bojja-s <bojja.s@hcl.com>
2018-06-14 19:50:20 +0530

    inital commit

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.bootcamp.fs.springboot.app.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.bootcamp.fs.springboot.app.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByemail(String username);
-}
