1dd90760a5ec580e55e5b34f192653a8af43f847
bojja-s <bojja.s@hcl.com>
2018-06-16 18:43:52 +0530

    updated for screens

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/service/UserServiceImpl.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/service/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.hcl.bootcamp.fs.springboot.app.jpa.UserRepository;
 import com.hcl.bootcamp.fs.springboot.app.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -30,4 +32,8 @@ public class UserServiceImpl implements UserService {
     public User findByUserName(String username) {
         return userRepository.findByUserName(username);
     }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}    
 }
