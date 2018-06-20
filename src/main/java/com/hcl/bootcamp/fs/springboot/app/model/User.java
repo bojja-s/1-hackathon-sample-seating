3c87014328671a02797d732df448c958f3f2838e
Bojja Saidaiah <bojja.s@hcl.com>
2018-06-20 20:28:38 +0530

    bla

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/model/User.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/model/User.java
@@ -30,12 +30,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name = "users")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
