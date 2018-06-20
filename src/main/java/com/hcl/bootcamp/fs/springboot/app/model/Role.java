ec482630fbfd5d461b5ed4a7a5a3747579141162
Bojja Saidaiah <bojja.s@hcl.com>
2018-06-20 19:31:27 +0530

    bla

--- src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Role.java
+++ src/main/java/com/hcl/bootcamp/fs/springboot/app/model/Role.java
@@ -1,23 +1,35 @@
 package com.hcl.bootcamp.fs.springboot.app.model;
+
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "roles")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
-	@ManyToMany(mappedBy = "roles")
-	private Collection<User> users;
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name="users_roles",
+    joinColumns={@JoinColumn(name="role_id", referencedColumnName="id")},
+    inverseJoinColumns={@JoinColumn(name="user_id", referencedColumnName="id")})
+    private Collection<User> users;
 }
